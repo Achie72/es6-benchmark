0952fcac34e17fd62c880887143456bb10610249
Bela Toth <tbela@inf.u-szeged.hu>
2019-08-12 11:36:38 +0200

    Add a map and array testing file
    Added a dungeon generator project from npm into the repository.
    Added proper licence header into the files.
    Added `expectedDungeonString.txt` containing the expected result of the generation using the `6` as seed.
    
    Signed-off-by: Bela Toth tbela@inf.u-szeged.hu

--- camelcase.js
+++ camelcase.js
@@ -1,15 +1,37 @@
 /*
-MIT License
-
-Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (sindresorhus.com)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ * MIT License
+ *
+ *  Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (sindresorhus.com)
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 'use strict';
+
+
+var assert = function(a,b) {
+    if (a == b) {
+        return true
+    } else {
+        throw "Error: Generated output does not correspond to the expected"
+    }
+}
+
 
 const preserveCamelCase = string => {
     let isLastCharLower = false;
@@ -81,14 +103,6 @@ const camelCase = (input, options) => {
 
     return postProcess(input);
 };
-
-var assert = function(a,b) {
-    if (a == b) {
-        print("true")
-    } else {
-        print("false")
-    }
-}
 
 const original_space_string = "Ignorant saw her her drawings marriage laughter. Case oh an that or away sigh \
 do here upon. Acuteness you exquisite ourselves now end forfeited. Enquire ye without it garrets up himself. \
